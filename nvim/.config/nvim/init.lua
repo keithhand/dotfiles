3b8c955de0d7286f131a132eb4856f7c2e31f423
keith.hand <rkeithhand@gmail.com>
2024-04-21 15:52:23 -0500

    add default spacing options

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,9 @@
+-- Set default spacing to 2 if none is recognized
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
