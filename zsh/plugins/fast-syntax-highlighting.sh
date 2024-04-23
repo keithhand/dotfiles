8a15a4676fa55fc80ae4362f798f3ad0f8f095f4
keith.hand <rkeithhand@gmail.com>
2024-04-23 03:09:58 -0500

    add zsh fsh

---
+++ zsh/plugins/fast-syntax-highlighting.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source $(brew --prefix)/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+
+# download and configure theme
+theme_url="https://raw.githubusercontent.com/catppuccin/zsh-fsh/main/themes/catppuccin-mocha.ini"
+file_name=$(basename $theme_url)
+theme_path=$XDG_CONFIG_HOME/fsh
+if [[ ! -f $theme_path/$file_name ]]; then
+  echo "missing zsh-fsh theme... downloading..."
+  mkdir -p $theme_path
+  wget -q $theme_url --output-document $theme_path/$file_name
+  echo "downloaded... updating theme..."
+  fast-theme XDG:catppuccin-mocha
+fi
