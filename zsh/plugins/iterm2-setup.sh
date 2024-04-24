8a88a610a3a94ff14fab6f862424531219e3460b
keith.hand <rkeithhand@gmail.com>
2024-04-24 15:16:51 -0500

    silence bell

--- zsh/plugins/iterm2-setup.sh
+++ zsh/plugins/iterm2-setup.sh
@@ -2,7 +2,7 @@
 
 if [[ $(uname) == "Darwin" ]]; then
   # Set iterm2 to use dotfile config by default
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$XDG_CONFIG_DIR/iterm2"
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES/iterm2"
   defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
   # Set up theme
