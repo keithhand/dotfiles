51c255e9f51b5693b707c8542d3e97ea4d2ea2ae
keith.hand <rkeithhand@gmail.com>
2024-04-24 14:59:37 -0500

    update up/down arrows to go to end of line

--- zsh/keymappings.sh
+++ zsh/keymappings.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 ## binds up and down arrow keys to history functions
-bindkey "^[[A" history-beginning-search-backward
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
 bindkey "^[[B" history-beginning-search-forward
