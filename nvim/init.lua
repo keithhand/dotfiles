75905b45b17c272c4fed2d562e66e1f1da41d546
keith.hand <rkeithhand@gmail.com>
2024-04-28 23:42:04 -0500

    remove leader f format keybind

--- nvim/init.lua
+++ nvim/init.lua
@@ -549,14 +549,14 @@ require("lazy").setup({
 		"stevearc/conform.nvim",
 		lazy = false,
 		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
+			-- {
+			-- 	"<leader>f",
+			-- 	function()
+			-- 		require("conform").format({ async = true, lsp_fallback = true })
+			-- 	end,
+			-- 	mode = "",
+			-- 	desc = "[F]ormat buffer",
+			-- },
 		},
 		opts = {
 			notify_on_error = false,
