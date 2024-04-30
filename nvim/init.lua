8dd68a8387116d0f322bef2cc7e66440c27fb34a
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-04-30 17:32:18 -0500

    add and config md and sh lint/formatters

--- nvim/init.lua
+++ nvim/init.lua
@@ -485,6 +485,7 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
+				bashls = {},
 				-- clangd = {},
 				-- gopls = {},
 				-- pyright = {},
@@ -527,6 +528,10 @@ require("lazy").setup({
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"markdownlint",
+				"mdformat",
+				"shellcheck",
+				"shfmt",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -572,6 +577,8 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				markdown = { "mdformat" },
+				sh = { "shfmt" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
