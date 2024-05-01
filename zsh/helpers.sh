74def8f8af71d2e291ab709e37d9b106cc64fd4b
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-04-30 19:59:11 -0500

    change git to config via fn instead of config

--- zsh/helpers.sh
+++ zsh/helpers.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
 git_clone() {
-  if [[ -z $1 ]]; then
-    echo "Please provide an http link to a git repository."
-    return
-  fi
-  url=$1
-  author=$(basename $(dirname $url))
-  repo=$(basename $url .git)
-  output_dir=$GIT_DIRECTORY/$author/$repo
-  git clone -q $url $output_dir
+	if [[ -z $1 ]]; then
+		echo "Please provide an http link to a git repository."
+		return
+	fi
+	url=$1
+	author=$(basename $(dirname $url))
+	repo=$(basename $url .git)
+	output_dir=$GIT_DIRECTORY/$author/$repo
+	git clone -q $url $output_dir
+}
+
+git_config() {
+	config_dir="$XDG_CONFIG_HOME/git"
+	mkdir -p "$config_dir"
+	touch "$XDG_CONFIG_HOME/git/config"
+
+	git_set() { git config --global "$1" "$2"; }
+	git_set user.name "keith.hand"
+	git_set user.email "43491549+keithhand@users.noreply.github.com"
+	git_set core.editor "vim"
+	git_set init.defaultBranch "main"
+	git_set commit.gpgsign true
 }
