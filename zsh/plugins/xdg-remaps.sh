1601616e14a0b69c0252058649704baf11f0bd6b
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-05-03 20:58:17 -0500

    fix kubeconfig

--- zsh/plugins/xdg-remaps.sh
+++ zsh/plugins/xdg-remaps.sh
@@ -9,11 +9,11 @@ export ANSIBLE_GALAXY_CACHE_DIR="${XDG_CACHE_HOME}/ansible/galaxy_cache"
 export GOPATH="$XDG_DATA_HOME"/go
 export GOMODCACHE="$XDG_CACHE_HOME"/go/mod
 
-export KUBECONFIG="$XDG_CONFIG_HOME/kube" 
+export KUBECONFIG="$XDG_CONFIG_HOME/kube/config"
 export KUBECACHEDIR="$XDG_CACHE_HOME/kube"
 
 export MINIKUBE_HOME="$XDG_DATA_HOME"/minikube
 
-export TLDR_CACHE_DIR=“$XDG_CACHE_HOME/tldrc”
+export TLDR_CACHE_DIR="$XDG_CACHE_HOME/tldrc"
 
 export LESSHISTFILE="$XDG_CACHE_HOME/less/history"
