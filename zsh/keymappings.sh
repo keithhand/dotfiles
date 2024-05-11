4596347109710d92788f44a4f084027be1acbb6c
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-05-11 12:38:19 -0500

    better up/down arrow keys

--- zsh/keymappings.sh
+++ zsh/keymappings.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 ## binds up and down arrow keys to history functions
-autoload -U history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^[[A" history-beginning-search-backward-end
-bindkey "^[[B" history-beginning-search-forward-end
-bindkey "^[[B" history-beginning-search-forward
+up="^[[A"
+down="^[[B"
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey $up up-line-or-beginning-search
+bindkey $down down-line-or-beginning-search
