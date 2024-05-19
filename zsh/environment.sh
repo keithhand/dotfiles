09591f64d17ab7b4b2bd40e5dc785befbfc73730
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-05-19 00:09:55 -0500

    linting

--- zsh/environment.sh
+++ zsh/environment.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p "$XDG_DATA_HOME/zsh"
-export GPG_TTY=$(tty)
+tty="$(tty)"
+export GPG_TTY=$tty
 export HISTFILE="$XDG_DATA_HOME/zsh/.zhistory"
 export HISTSIZE=10000
 export SAVEHIST=10000
--- zsh/keymappings.sh
+++ zsh/keymappings.sh
@@ -7,5 +7,5 @@ autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey $up up-line-or-beginning-search
-bindkey $down down-line-or-beginning-search
+bindkey "$up" up-line-or-beginning-search
+bindkey "$down" down-line-or-beginning-search
