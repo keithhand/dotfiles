30bd99103623756964a3229774689fcb0203c490
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-05-19 15:39:00 -0500

    refactor(git): changes git-config to be based on a file

--- modules/zsh/helpers.sh
+++ modules/zsh/helpers.sh
@@ -11,16 +11,3 @@ git_clone() {
 	output_dir=$GIT_DIRECTORY/$author/$repo
 	git clone -q $url $output_dir
 }
-
-git_config() {
-	config_dir="$XDG_CONFIG_HOME/git"
-	mkdir -p "$config_dir"
-	touch "$XDG_CONFIG_HOME/git/config"
-
-	git_set() { git config --global "$1" "$2"; }
-	git_set user.name "keith.hand"
-	git_set user.email "43491549+keithhand@users.noreply.github.com"
-	git_set core.editor "vim"
-	git_set init.defaultBranch "main"
-	git_set commit.gpgsign true
-}
