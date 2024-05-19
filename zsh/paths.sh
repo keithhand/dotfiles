e1cde857288cbe44ce7c2c4f4f936db46da18018
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-05-19 00:09:35 -0500

    remove paths

--- zsh/paths.sh
+++ zsh/paths.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
 addToPath() {
-  if [[ "$PATH" != *"$1"* ]]; then
-    export PATH=$PATH:$1
-  fi
+	if [[ "$PATH" != *"$1"* ]]; then
+		export PATH=$PATH:$1
+	fi
 }
 
 addToPathFront() {
-  if [[ "$PATH" != *"$1"* ]]; then
-    export PATH=$1:$PATH
-  fi
+	if [[ "$PATH" != *"$1"* ]]; then
+		export PATH=$1:$PATH
+	fi
 }
 
-addToPathFront "$HOME/bin:/usr/local/bin"
 addToPathFront "$HOME/.local/bin"
-addToPathFront "$HOME/.local/scripts"
-addToPathFront "$HOME/.kubectx"
+# TODO: check on windows if I can remove these?
 addToPath "/mnt/c/Windows/System32/WindowsPowerShell/v1.0"
 addToPath "/mnt/c/Users/rkeit/AppData/Local/Programs/Microsoft VS Code/bin"
 
