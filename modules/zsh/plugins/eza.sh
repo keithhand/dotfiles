a6d589c4e529543cbda58fd745de6ab59ae3cdb2
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-06-19 03:13:22 -0500

    fix(zsh): sorts ls by Name

--- modules/zsh/plugins/eza.sh
+++ modules/zsh/plugins/eza.sh
@@ -2,8 +2,8 @@
 
 # eza - custom ls
 # shellcheck disable=SC2139
-alias lso >/dev/null 2>&1 || alias lso="$(which ls)"
-alias ls="eza --color=auto --icons=auto --group-directories-first --across"
+alias lso >/dev/null 2>&1 || alias lso='eval $(which ls)'
+alias ls="eza --color=auto --icons=auto --group-directories-first --across --sort Name"
 alias ll="ls --long -g --smart-group --git --time-style=relative --header -F"
 alias la="ll --all"
 alias lp="la --tree --ignore-glob=.git --git-ignore --total-size"
