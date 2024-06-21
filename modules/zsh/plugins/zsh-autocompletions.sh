1cfc905a31d04cadf14a40dd053dc829288f7ed2
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-06-21 11:26:23 -0500

    fix(zsh): moves autocompletions to .zshrc

--- modules/zsh/plugins/zsh-autocompletions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# zsh autocompletions
-autoload -Uz compinit
-fpath=(/srv/git/zsh-users/zsh-completions/src $fpath)
-compdump=$XDG_DATA_HOME/zsh/.zcompdump
-compinit -d $compdump &>/dev/null
