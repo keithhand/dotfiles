ca713dd51e2c9aa43d6a5c21cb4e344811ae1134
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-07-21 16:55:41 -0500

    refactor(environment): moves some env sets

--- modules/zsh/environment.sh
+++ modules/zsh/environment.sh
@@ -2,6 +2,7 @@
 
 mkdir -p "$XDG_DATA_HOME/zsh"
 tty="$(tty)"
+text_editor="nvim"
 export GPG_TTY=$tty
 export HISTFILE="$XDG_DATA_HOME/zsh/.zhistory"
 export HISTSIZE=10000
@@ -9,3 +10,9 @@ export SAVEHIST=10000
 export THEME="catppuccin-mocha"
 export UID="$UID"
 export GID="$GID"
+export VISUAL="$text_editor"
+export EDITOR="$text_editor"
+export KUBE_EDITOR="$text_editor"
+export GIT_EDITOR="$EDITOR"
+export DOTFILES="$HOME/.dotfiles"
+export OBSIDIAN_DIRECTORY="$HOME/obsidian/personal-vault"
